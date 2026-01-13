c5012719b7269f767b052d4c17936b7db638f8a9
acarrillocr <81390103+andresc452@users.noreply.github.com>
2026-01-12 23:12:12 -0600

    Add new data

---
+++ scripts/init_database.sql
@@ -0,0 +1,34 @@
+/*
+Create DB and Schemas
+
+Script Purpose:
+
+WARNING:
+    Running this script will drop the entire 'DataWarehouse'
+*/
+
+USE master;
+GO
+
+-- Drop and recreate the 'DataWarehouse' DB
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- Create the 'DataWarehouse' DB
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
